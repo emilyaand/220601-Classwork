e77ca309792dbb99dea21691dbce00756b2bf166
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-06-01 21:33:25 +0400

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _220601_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)      
+        {
+            FlyObject(new Bird());
+            FlyObject(new Plane());
+        }
+
+        static void FlyObject(IFliable fliable)
+        {
+            fliable.Fly();
+        }
+    }
+    interface IFliable
+    {
+        public abstract void Fly();
+    }
+    class Bird : IFliable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+    }
+
+    class Plane : IFliable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Plane is flying");
+        }
+
+
+    }
+}
+
+ 
